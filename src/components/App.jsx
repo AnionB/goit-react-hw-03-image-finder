1f0448b216cf04f15496d4da52f6a0bbf8872688
AnionB <90142322+AnionB@users.noreply.github.com>
2022-04-20 16:45:28 +0300

    create blank page

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+
+// import './styles.css';
+
+export class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <Searchbar></Searchbar>
+      </div>
+    );
+  }
+}
+
+{
+  /* <ImageGallery> */
+}
+{
+  /* <ImageGalleryItem> */
+}
+{
+  /* <Loader> */
+}
+{
+  /* <Button> */
+}
+{
+  /* <Modal></Modal> */
+}
