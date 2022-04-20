2a8eae60a42af7a6d1de8ff978f48bb6b4a734c9
AnionB <90142322+AnionB@users.noreply.github.com>
2022-04-20 17:18:15 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 
-// import './styles.css';
-
 export class App extends Component {
   state = {};
 
   render() {
     return (
       <div>
-        <Searchbar></Searchbar>
+        <Searchbar />
       </div>
     );
   }
-}
-
-{
-  /* <ImageGallery> */
-}
-{
-  /* <ImageGalleryItem> */
-}
-{
-  /* <Loader> */
-}
-{
-  /* <Button> */
-}
-{
-  /* <Modal></Modal> */
 }
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { MdSearch } from 'react-icons/md';
 // import styles from './Searchbar.module.css';
 
