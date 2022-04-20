37ffef4ef773b693fde66ed9184286211bfead64
AnionB <90142322+AnionB@users.noreply.github.com>
2022-04-20 16:54:02 +0300

    update

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { MdSearch } from 'react-icons/md';
+import styles from './Searchbar.module.css';
+
+export default class Searchbar extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  render() {
+    return (
+      <form>
+        <button>
+          <MdSearch />
+        </button>
+        <input></input>
+      </form>
+    );
+  }
+}
+// Searchbar.propTypes = {
+//   contact: PropTypes.func,
+// };
